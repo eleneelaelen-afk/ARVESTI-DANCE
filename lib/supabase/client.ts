d3c5679b1e8099af8f9b4dcbfebf0fe230751b65
eleneelaelen-afk <eleneelaelen@gmail.com>
2026-09-25 02:14:16 +0300

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-// Вставьте сюда ваши реальные данные:
-const SUPABASE_URL = 'https://https://oeexjdkmwconsjyjvpia.supabase.co';
+const SUPABASE_URL = 'https://oeexjdkmwconsjyjvpia.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_NqK4ipK1jds2eaPln0QvEg_4yyGy2YC';
 
 export function createClient() {
